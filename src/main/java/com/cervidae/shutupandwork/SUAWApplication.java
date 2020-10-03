c04b966b6fc452306a700c688e1edf074486eeec
hellodrf <r.du1@student.unimelb.edu.au>
2020-10-03 17:08:15 +0800

    project changed

--- src/main/java/com/cervidae/shutupandwork/SUAWApplication.java
+++ src/main/java/com/cervidae/shutupandwork/SUAWApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan
 @SpringBootApplication
-public class CbookingsApplication {
+public class SUAWApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CbookingsApplication.class, args);
+        SpringApplication.run(SUAWApplication.class, args);
     }
 
 }
--- src/test/java/com/cervidae/shutupandwork/SUAWApplicationTests.java
+++ src/test/java/com/cervidae/shutupandwork/SUAWApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CbookingsApplicationTests {
+class SUAWApplicationTests {
 
     @Test
     void contextLoads() {
