ff410f209f2bddae8a8d265f77c4e1154f17b3d3
hellodrf <r.du1@student.unimelb.edu.au>
2020-11-03 15:00:06 +0800

    DDL update

--- src/main/java/com/cervidae/shutupandwork/config/AsyncConfig.java
+++ src/main/java/com/cervidae/shutupandwork/config/AsyncConfig.java
@@ -2,12 +2,13 @@ package com.cervidae.shutupandwork.config;
 
 import com.cervidae.shutupandwork.controller.ServerExceptionHandler;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
-
+@Configuration
 public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
